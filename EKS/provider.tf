28f029d557c8e9d4d67bff840f74953e416289ff
sathish103 <sathishreddys786@gmail.com>
2025-06-21 17:59:49 +0530

    proveider version added

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -11,6 +11,21 @@ locals {
   }
 }
 
+
+
+terraform {
+  required_version = ">= 1.12.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33.0"  # known stable version
+    }
+  }
+}
+
+
+
 provider "aws" {
   region = "ap-south-1"
 }
